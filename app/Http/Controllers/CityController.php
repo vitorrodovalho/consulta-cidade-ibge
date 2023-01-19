a05eeb204588a889a3cf1fe74a59bc0d0167de15
Vitor Toledo <vitortoledorodovalho@gmail.com>
2023-01-19 10:24:51 -0300

    Implementação cache redis para api request municipios

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class CityController extends Controller
 {
@@ -12,11 +13,32 @@ class CityController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($state)
+    public function index(Request $request, $state)
     {
-        return City::select('cities.name', 'cities.ibge_id as ibge_code')
-            ->where('acronym', $state)
-            ->join('states', 'states.id', '=', 'state_id')
-            ->paginate();
+        if ($request->has('page'))
+            $page = $request->page;
+        else
+            $page = 1;
+
+        $expiration = 1440; // Tempo em minutos para expirar cache
+        $key = 'cities_' . $page;
+
+        return Cache::remember($key, $expiration, function () use ($state, $request){
+            $qb = City::query();
+            $qb->select('cities.name', 'cities.ibge_id as ibge_code');
+            $qb->where('acronym', $state);
+
+            if ($request->has('q')) {
+                $qb->where(function($query) use ($qb, $request) {
+                    $query->where('cities.name', 'Like', '%' . $request->q. '%');
+                });
+            }
+
+            if ($request->has('sortBy'))
+                $qb->orderBy($request->get('sortBy'), $request->get('direction', 'ASC'));
+
+            $qb->join('states', 'states.id', '=', 'state_id');
+            return $qb->paginate();
+        });
     }
 }
