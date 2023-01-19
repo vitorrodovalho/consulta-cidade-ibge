a9bb3a1a494edddfd37ae198fb46b9a489b884b6
Vitor Toledo <vitortoledorodovalho@gmail.com>
2023-01-19 08:50:35 -0300

    Implementação service provider cidades

--- app/Providers/StatesAndCitiesServiceProvider.php
+++ app/Providers/StatesAndCitiesServiceProvider.php
@@ -3,26 +3,38 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Kayo\StatesAndCitiesIbge\Services\Integration\IbgeRestIntegrationService;
 
 class StatesAndCitiesServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        $this->publishConfig();
+        $this->registerCommands();
+    }
+
+    public function register()
+    {
+        $this->app->singleton(IbgeRestIntegrationService::class, function ($app) {
+            return new IbgeRestIntegrationService(
+                config('ibge.integration.host', 'http://localhost:8000'),
+                config('ibge.integration.timeout', 20)
+            );
+        });
+    }
+
+    private function registerCommands()
+    {
+    }
+
+    private function publishConfig()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/ibge.php' => config_path('ibge.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
+        ], 'migrations');
     }
 }
